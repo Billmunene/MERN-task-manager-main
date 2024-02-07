d2510d5ec7ed957bdce7dc1c8b682d22949f3ec0
Bill Munene <billmunene937@gmail.com>
2024-02-07 04:46:44 +0300

    null
    nff

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
  1 npm run install-install
  2 edit the .env with your database url
  3  npm run dev
+
+
+create user and access site
+
+
+to access admin features
+change user role in user schema to admin
---
+++ tests/test.js
@@ -0,0 +1,9 @@
+    // Router should be created successfully
+    it('should create router successfully', () => {
+      const express = require("express");
+      const router = express.Router();
+      const { getTasks, getTask, postTask, putTask, deleteTask } = require("../controllers/taskControllers");
+      const { verifyAccessToken } = require("../middlewares.js");
+
+      expect(router).toBeDefined();
+    });
